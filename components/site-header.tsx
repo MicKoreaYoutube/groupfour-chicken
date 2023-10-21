8928cdf9c9ab3729959f174cdcc3d4ad37977bc1
MicKoreaYoutube <118183460+MicKoreaYoutube@users.noreply.github.com>
2023-10-21 07:11:37 +0000

    되겠지

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -32,38 +32,6 @@ export function SiteHeader() {
       <div className="container flex h-16 items-center space-x-4 sm:justify-between sm:space-x-0">
         <MainNav items={siteConfig.mainNav} />
         <div className="flex flex-1 items-center justify-end space-x-4">
-          <Sheet>
-            <SheetTrigger asChild>
-              <Button variant="ghost" className="block sm:hidden"><Menu className="h-4 w-4" /></Button>
-            </SheetTrigger>
-            <SheetContent>
-              <SheetHeader className="mb-4">
-                <SheetTitle className="text-2xl font-KBO-Dia-Gothic_bold">{siteConfig.name}</SheetTitle>
-                <SheetDescription className="text-md font-SUITE-Regular">
-                  {siteConfig.description}
-                </SheetDescription>
-              </SheetHeader>
-              {items?.length ? (
-                <nav className="flex flex-col gap-2 font-SUITE-Regular">
-                  {items?.map(
-                    (item: any, index: any) =>
-                      item.href && (
-                        <Link
-                          key={index}
-                          href={item.href}
-                          className={cn(
-                            "flex items-center text-md font-medium text-muted-foreground",
-                            item.disabled && "cursor-not-allowed opacity-80"
-                          )}
-                        >
-                          {item.title}
-                        </Link>
-                      )
-                  )}
-                </nav>
-              ) : null}
-            </SheetContent>
-          </Sheet>
         </div>
       </div>
     </header>
