38e767250665eba2927668b2f1d32bb0113b4168
MicKoreaYoutube <118183460+MicKoreaYoutube@users.noreply.github.com>
2023-10-21 10:36:06 +0900

    Initial commit

---
+++ config/site.ts
@@ -0,0 +1,20 @@
+export type SiteConfig = typeof siteConfig
+
+export const siteConfig = {
+  name: "Next.js Template",
+  description:
+    "A nextjs template by MicKoreaYoutube based on shadcn-ui",
+  mainNav: [
+    {
+      title: "Home",
+      href: "/",
+    },
+  ],
+  links: {
+    shadcnTwitter: "https://twitter.com/shadcn",
+    shadcnGithub: "https://github.com/shadcn/ui",
+    shadcnuiDocs: "https://ui.shadcn.com",
+    micInstagram: "https://instagram.com/leejunsibal",
+    micGithub: "https://github.com/MicKoreaYoutube/mic-next-template"
+  },
+}
